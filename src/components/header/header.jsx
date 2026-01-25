0aeb78590b0a6ff22aa4dbb4032774ee12fc2322
KelCyberTech <kelvinodamtten@gmail.com>
2026-01-25 22:31:30 +0000

    css styling and header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Menu, Search} from 'lucide-react'
+import {Filter, Menu, Search, Plus, Sun, Bell, Settings} from 'lucide-react'
 
 function Header(){
 
@@ -20,7 +20,7 @@ function Header(){
                 </div>
             </div>
 
-            {/* Center section */}
+            {/* Center section - Search bar */}
             <div className='flex-1 max-w-md mx-8'>
                 <div className='relative'>
                     <Search className='w-4 h-4 absolute left-3 top-1/2 transform -translate-y-1/2 text-slate-400'/>
@@ -28,9 +28,53 @@ function Header(){
                         'bg-slate-100 dark:bg-slate-800 border border-slate-200 dark:border-slate-700 ' +
                         'text-slate-800 dark:text-white placeholder-slate-500 focus:outline-none focus:ring-2' +
                         'focus:ring-blue-500 focus:border-transparent transition-all'}/>
+                    <button className={'absolute right-2 top-1/2 transform -translate-y-1/2 p-1.5 text-slate-400 ' +
+                        'hover:text-slate-600 dark:hover:text-slate-300'}>
+                        <Filter />
+                    </button>
                 </div>
             </div>
 
+            {/* Right */}
+            <div className='flex items-center space-x-3'>
+                {/*Quick Action Button*/}
+                <button className='hidden lg:flex items-center space-x-2 py-2 bg-linear-to-r px-4 from-blue-500
+                to-purple-600 text-white rounded-xl hover:shadow-lg transition-a'>
+                    <Plus className='w-5 h-5 cursor-pointer'/>
+                    <span className='text-sm font-medium cursor-pointer'>New</span>
+                </button>
+            </div>
+            {/* Toggle*/}
+            <button className={'p-2.5 rounded-xl text-slate-600 hover:bg-slate-100 dark:text-slate-300 dark:hover:bg-slate-800  transition-colors'}>
+                <Sun className='w-5 h-5' />
+            </button>
+
+            {/*Notification*/}
+            <button className='relative p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100
+            dark:hover:bg-slate-800 transition-colors'>
+                <Bell className='w-5 h-5' />
+                <span className='absolute -top-1 w-5 h-5 bg-red-500 text-white text-xs rounded-full flex
+                items-center justify-center'>3</span>
+            </button>
+
+            {/*Settings*/}
+            <button className={'p-2.5 rounded-xl text-slate-600 dark:text-slate-300 hover:bg-slate-100 ' +
+                'dark:hover:bg-slate-800 transition-colors'}>
+                <Settings className='w-5 h-5 dark:text-slate-300' />
+            </button>
+
+            {/*User profile*/}
+            <div className='flex items-center space-x-3'>
+                <img src={''} alt='user profile' className='w-10 h-10 rounded-full ring-2 ring-blue-500 object-cover'/>
+                <div className={'flex-1 min-w-0'}>
+                    <p className='text-sm font-medium text-slate-800 dark:text-white'>
+                        Alex Johnson
+                    </p>
+                    <p className='text-xs text-slate-500 dark:text-slate-400 truncate'>
+                        Admin
+                    </p>
+                </div>
+            </div>
         </div>
     </div>
 
